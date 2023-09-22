2415449ee69032ca1ba6efa07722451e9f95a852
Eduardo Felipe <86427741+ltzEdu@users.noreply.github.com>
2023-09-21 21:59:50 -0300

    ltzedu2.js
    o segundo passo com java script

---
+++ ltzedu2.js
@@ -0,0 +1,20 @@
+function setup() {
+
+  createCanvas(700, 700);
+
+  background("black");
+
+}
+
+function draw() {
+
+stroke("blue");
+         
+fill("red");
+  
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 20, 20);
+  
+}
+}
